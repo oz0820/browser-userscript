418e62ff44e986fcd1af631b8b45b2d520fea0db
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-01-01 14:33:04 +0900

    URL変更の検出方法を変更

--- youtube-thumbnail-button/youtube-thumbnail-button.user.js
+++ youtube-thumbnail-button/youtube-thumbnail-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Thumbnail Button
 // @namespace    https://twitter.com/oz0820
-// @version      2023.01.01.3
+// @version      2023.01.01.4
 // @description  Youtubeの再生ウィンドウにサムネイル直行ボタンを追加すると思います。
 // @author       oz0820
 // @match        https://www.youtube.com/*
@@ -11,13 +11,18 @@
 
 (function () {
     let thumbnail_url = "";
+    let href = window.location.href;
 
-    // Youtubeのページを移動したときに発火します
-    document.addEventListener('transitionend', function(e) {
-        if (e.target.id === 'progress') {
+    // ページ移動を検出します
+    const observer = new MutationObserver(function () {
+        if (href !== window.location.href) {
+            href = window.location.href;
+            // 処理
+            console.log("【YTB】 URL Changed.")
             set_extended_thumbnail();
         }
-    });
+    })
+    observer.observe(document, { childList: true, subtree: true })
 
     function set_url() {
         let param_search = new URLSearchParams(window.location.search);
@@ -62,7 +67,7 @@
     async function init() {
         for (let i = 0; i < 50; i++) {
             try {
-                // 雑にボタン追加して
+                // 雑にボタン追加します
                 document.getElementsByClassName('ytp-right-controls')[0].insertAdjacentHTML('afterbegin', '<button class="ytp-controls" type="button" id="show_thumbnail_button" aria-label="サムネイルを表示する" title="サムネイルを表示する" style="background: none; fill: white; border: none; cursor: pointer; float: left; outline: none; overflow: visible; padding: 0px 0px 0em; width: 3em;"><svg viewBox="0 0 16 16" style="width: 60%;"><path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path><path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"></path></svg></button>');
                 break;
             } catch (e) {
@@ -75,10 +80,11 @@
         document.getElementById('show_thumbnail_button').addEventListener('click', function () {
             window.open(thumbnail_url)
         });
-        // サムネイルを埋め込んで
+        // サムネイルを埋め込みます
         set_extended_thumbnail()
     }
     init();
 
 })();
+
 
