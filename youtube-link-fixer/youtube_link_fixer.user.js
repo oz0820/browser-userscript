7fe8e15c8fb8cf3307fb9fea58a3afe6a30b3a40
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-06-10 03:37:32 +0900

    ページ移動を強制させた

--- youtube-link-fixer/youtube_link_fixer.user.js
+++ youtube-link-fixer/youtube_link_fixer.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         YouTube Link Fixer
 // @namespace    https://twitter.com/oz0820
-// @version      2023.6.10.1
-// @description  「○○週間」とか言って左上のYoutubeトップへのリンクを書き換えられて困っている方向け。
+// @version      2023.6.10.2
+// @description  「○○週間」とか言って左上のYoutubeトップへのリンクを書き換えられて困っている方向け。強引な実装なので重たいです。
 // @author       oz0820
 // @match        https://www.youtube.com/*
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/youtube_link_fixer/youtube_link_fixer.user.js
@@ -11,7 +11,11 @@
 
 (function() {
     let elm = document.getElementsByClassName('yt-simple-endpoint style-scope ytd-topbar-logo-renderer')[0];
-    elm.href = 'https://www.youtube.com/';
-    elm.title = 'YouTube ホーム';
-    elm.innerHTML = '<div class="style-scope ytd-topbar-logo-renderer"><ytd-logo class="style-scope ytd-topbar-logo-renderer" is-red-logo=""></ytd-logo></div>';
+    if (elm.href !== "https://www.youtube.com/") {
+        elm.href = '/';
+        elm.setAttribute("onclick", "window.location.href = '/'");
+        // ロゴを無理矢理戻すなら実行する
+        // elm.innerHTML = '<div class="style-scope ytd-topbar-logo-renderer"><ytd-logo class="style-scope ytd-topbar-logo-renderer" is-red-logo=""></ytd-logo></div>';
+    }
+
 })();
