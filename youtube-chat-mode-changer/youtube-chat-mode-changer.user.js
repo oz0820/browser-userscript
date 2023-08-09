d7ffe289aadd35fca115d06ec89d0e7219df7595
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-08-09 09:04:00 +0900

    動作するPathを変更

--- youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
+++ youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Youtube chat mode changer
 // @namespace    https://twitter.com/oz0820
-// @version      2023.08.08.3
+// @version      2023.08.09.0
 // @description  Youtubeのチャットのリプレイを常に全て表示します．
 // @author       oz0820
-// @match        https://www.youtube.com/*
+// @match        https://www.youtube.com/live_chat*
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
 // @icon         data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTU5IiBoZWlnaHQ9IjExMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCiAgPHBhdGggZD0ibTE1NCAxNy41Yy0xLjgyLTYuNzMtNy4wNy0xMi0xMy44LTEzLjgtOS4wNC0zLjQ5LTk2LjYtNS4yLTEyMiAwLjEtNi43MyAxLjgyLTEyIDcuMDctMTMuOCAxMy44LTQuMDggMTcuOS00LjM5IDU2LjYgMC4xIDc0LjkgMS44MiA2LjczIDcuMDcgMTIgMTMuOCAxMy44IDE3LjkgNC4xMiAxMDMgNC43IDEyMiAwIDYuNzMtMS44MiAxMi03LjA3IDEzLjgtMTMuOCA0LjM1LTE5LjUgNC42Ni01NS44LTAuMS03NXoiIGZpbGw9IiNmMDAiIC8+DQogIDxwYXRoIGQ9Im0xMDUgNTUtNDAuOC0yMy40djQ2Ljh6IiBmaWxsPSIjZmZmIiAvPg0KPC9zdmc+
 // ==/UserScript==
