9d2920115a565b36decc8a33f217ffbdaf785a72
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-09-25 16:09:47 +0900

    Youtube original channel idを追加

---
+++ youtube-original-channel-id/README.md
@@ -0,0 +1,12 @@
+# Youtube original channel id
+
+YoutubeのチャンネルページにチャネルIDを表示するボタンを追加します。
+
+### インストール
+- [tampermonkey](https://www.tampermonkey.net/)とかユーザースクリプトを実行できる拡張機能をインスコする。
+- [ここ](https://github.com/oz0820/browser-userscript/raw/main/youtube-original-channel-id/youtube-original-channel-id.user.js)(このプロジェクトの最新コード)押す。
+- 指示に従ってインストール。
+
+#### 既知の問題
+- チャンネルページを新規にバックグラウンドで開いたとき、正常に動作しない。
+
---
+++ youtube-original-channel-id/youtube-original-channel-id.user.js
@@ -0,0 +1,135 @@
+// ==UserScript==
+// @name            Youtube original channel id
+// @namespace       https://twitter.com/oz0820
+// @version         2023.09.25.0
+// @description     YoutubeのチャンネルIDを表示する機能を追加します
+// @author          oz0820
+// @match           https://www.youtube.com/*
+// @updateURL       https://github.com/oz0820/browser-userscript/raw/main/youtube-original-channel-id/youtube-original-channel-id.user.js
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// ==/UserScript==
+
+(function () {
+
+    // ページ移動を検出します
+    let href = window.location.href;
+    let c_handle = '';
+    const observer = new MutationObserver(async function () {
+        if (href !== window.location.href) {
+            href = window.location.href;
+
+            if (!href.split('/')[3].startsWith('@')) {
+                return;
+            }
+
+            if (c_handle !== href.split('/')[3]) {
+                c_handle = href.split('/')[3];
+                await init();
+            }
+        }
+    })
+    observer.observe(document, { childList: true, subtree: true });
+
+    // ハンドルを使ったチャンネルページのみ実行する
+    if (!window.location.href.split('/')[3].startsWith('@')) {
+        return;
+    }
+
+    window.onload = async function () {
+        await init();
+    }
+
+
+    async function init() {
+        const cid = await get_og_cid();
+
+        if (!cid) {
+            console.log('cid error');
+            return;
+        }
+
+        console.log('window.onload', document.querySelectorAll('[id="channel-handle"]').length);
+        // 念のため削除
+        document.querySelectorAll('.yt_og_chanel_id').forEach((elm) => {
+            elm.remove();
+        });
+
+        const ex_html = `
+<yt-formatted-string id="channel-id" class="delimiter style-scope ytd-c4-tabbed-header-renderer">cannot_get_channel_id</yt-formatted-string>
+<button value="change" class="yt_og_chanel_id" id="change_button" style="padding-top: 0; padding-bottom: 0; margin-left: 10px;">change</button>`;
+
+        document.querySelectorAll('[id="channel-handle"]').forEach((elm) => {
+            elm.insertAdjacentHTML('afterend', ex_html);
+        });
+
+        document.querySelectorAll('#change_button.yt_og_chanel_id').forEach((elm) => {
+            elm.addEventListener('click', function () {
+                yt_og_cid();
+            })
+        })
+
+        document.querySelectorAll('yt-formatted-string[id="channel-id"]').forEach((elm) => {
+            elm.innerHTML = cid;
+            elm.removeAttribute('is-empty');
+        });
+
+        // ハンドルを表示にして
+        document.querySelectorAll('yt-formatted-string#channel-handle.delimiter').forEach((elm) => {
+            elm.classList.remove('delimiter');
+        })
+        // cidを非表示にする
+        document.querySelectorAll('yt-formatted-string#channel-id').forEach((elm) => {
+            elm.classList.add('delimiter');
+        })
+    }
+
+
+    function yt_og_cid() {
+
+        // ハンドルが非表示にされていたら
+        if (document.querySelector('yt-formatted-string#channel-handle.delimiter')) {
+            // ハンドルを表示にして
+            document.querySelectorAll('yt-formatted-string#channel-handle.delimiter').forEach((elm) => {
+                elm.classList.remove('delimiter');
+            })
+            // cidを非表示にする
+            document.querySelectorAll('yt-formatted-string#channel-id').forEach((elm) => {
+                elm.classList.add('delimiter');
+            })
+
+        } else {
+            // ハンドルを非表示にして
+            document.querySelectorAll('yt-formatted-string#channel-handle').forEach((elm) => {
+                elm.classList.add('delimiter');
+            })
+            // cidを表示する
+            document.querySelectorAll('yt-formatted-string#channel-id.delimiter').forEach((elm) => {
+                elm.classList.remove('delimiter');
+            })
+        }
+    }
+
+
+    async function get_og_cid() {
+        try {
+            const response = await fetch(location.href);
+            console.log(response.url);
+            const html = await response.text();
+
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+
+            let og_url = doc.querySelector('meta[property="og:url"]').getAttribute('content');
+            const cid = og_url.split('/')[4];
+            if (!cid) {
+                throw new Error('URLの解析に失敗しました。');
+            }
+            return cid;
+        } catch (error) {
+            console.error('データの取得に失敗しました:', error);
+            return null;
+        }
+    }
+
+})();
+
