161a9a04d2e97297ca7f57cb2a3227a4159d26c0
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-11-15 00:41:14 +0900

    新ツイート表示ボタンに色を付ける機能を追加

--- twitter-custom-style/twitter-custom-style.user.js
+++ twitter-custom-style/twitter-custom-style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Twitter custom style
 // @namespace       https://twitter.com/oz0820
-// @version         2023.11.14.0
+// @version         2023.11.15.0
 // @description     Twitterにオレオレスタイルを適用します．
 // @author          oz0820
 // @match           https://twitter.com/*
@@ -11,6 +11,14 @@
 // ==/UserScript==
 
 (function() {
-    const add_style = `<style>div.css-1dbjc4n.r-1kihuf0.r-13qz1uu {display: none;}</style>`;
+    const add_style = `<style>
+div.css-1dbjc4n.r-1kihuf0.r-13qz1uu {
+    display: none;
+}
+div.css-18t94o4.css-1dbjc4n.r-1777fci.r-1pl7oy7.r-1ny4l3l.r-o7ynqc.r-6416eg.r-13qz1uu {
+    background-color: aqua;
+}
+</style>`;
+
     document.head.insertAdjacentHTML('beforeend', add_style);
 })();
