c11be7556e35394e31d6c2485efe66d5bddb187b
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-11-25 11:56:43 +0900

    チャンネルIDを常時表示に変更

--- youtube-original-channel-id/youtube-original-channel-id.user.js
+++ youtube-original-channel-id/youtube-original-channel-id.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube original channel id
 // @namespace       https://twitter.com/oz0820
-// @version         2023.10.27.0
+// @version         2023.11.25.0
 // @description     YoutubeのチャンネルIDを表示する機能を追加します
 // @author          oz0820
 // @match           https://www.youtube.com/*
@@ -12,123 +12,152 @@
 (function () {
 
     // ページ移動を検出します
-    let href = window.location.href;
-    let c_handle = '';
+    let path = location.pathname
+    let channel_handle = ''
     const observer = new MutationObserver(async function () {
-        if (href !== window.location.href) {
-            href = window.location.href;
+        if (path !== location.pathname) {
+            path = location.pathname
 
-            if (!href.split('/')[3].startsWith('@') && !(href.split('/')[3] === 'channel')) {
-                return;
+            // チャンネルページ以外では実行しない
+            if (!location.pathname.startsWith('/@') && location.pathname !== '/channel') {
+                return
             }
 
-            if (c_handle !== href.split('/')[3]) {
-                c_handle = href.split('/')[3];
-                await init();
+            // チャンネル別チャンネルに移動したときだけ実行する
+            if (channel_handle !== location.pathname.split('/')[1]) {
+                channel_handle = location.pathname.split('/')[1]
+                await init()
             }
         }
     })
-    observer.observe(document, { childList: true, subtree: true });
+    observer.observe(document, { childList: true, subtree: true })
 
-    // チャンネルページのみ実行する
-    if (!href.split('/')[3].startsWith('@') && !(href.split('/')[3] === 'channel')) {
-        return;
+    // チャンネルページ以外では実行しない
+    if (!location.pathname.startsWith('/@') && location.pathname !== '/channel') {
+        return
     }
 
-    window.onload = async function () {
-        await init();
+    window.onload = async () => {
+        await init()
     }
 
 
-    async function init() {
-        const cid = await get_og_cid();
+    const init = async () => {
+        const cid = await original_channel_id()
 
         if (!cid) {
-            console.log('cid error');
-            return;
+            logger.error('ChannelIDが見つかりません')
+            return
         }
-
         // 念のため削除
-        document.querySelectorAll('.yt_og_chanel_id').forEach((elm) => {
-            elm.remove();
-        });
+        document.querySelectorAll('.yt_og_channel_id').forEach((elm) => {
+            elm.remove()
+        })
 
         const ex_html = `
-<yt-formatted-string id="channel-id" class="style-scope ytd-c4-tabbed-header-renderer yt_og_chanel_id" style="font-weight: 500;">cannot_get_channel_id</yt-formatted-string>
-<button value="change" class="yt_og_chanel_id" id="member_list" style="padding-top: 0; padding-bottom: 0; margin-left: 10px;">Member list</button>`;
+<span class="meta-item style-scope ytd-c4-tabbed-header-renderer yt_og_channel_id">
+    <yt-formatted-string id="channel-id" class="style-scope ytd-c4-tabbed-header-renderer yt_og_channel_id">cannot_get_channel_id</yt-formatted-string>
+    <span aria-hidden="true" class="delimiter style-scope ytd-c4-tabbed-header-renderer">‧</span>
+</span>
 
+<span class="meta-item style-scope ytd-c4-tabbed-header-renderer yt_og_channel_id">
+    <button value="change" class="yt_og_channel_id" id="member_list">Member list</button>
+    <span aria-hidden="true" class="delimiter style-scope ytd-c4-tabbed-header-renderer">‧</span>
+</span>
+`
 
         // チャンネルのハンドルを表示する要素の後ろに ex_html を挿入する
-        document.querySelectorAll('[id="channel-handle"]').forEach((elm) => {
-            elm.insertAdjacentHTML('afterend', ex_html);
-        });
+        document.querySelectorAll('div#inner-header-container span.ytd-c4-tabbed-header-renderer yt-formatted-string#channel-handle')
+            .forEach(elm => {
+                elm.parentElement.insertAdjacentHTML('afterend', ex_html)
+            })
 
         // 追加したボタンに操作用のEventListenerを追加
-        document.querySelectorAll('#member_list.yt_og_chanel_id').forEach((elm) => {
+        document.querySelectorAll('button#member_list.yt_og_channel_id').forEach((elm) => {
             elm.addEventListener('click', function () {
-                yt_open_member_list();
+                yt_open_member_list()
             })
         })
 
+
         // 要素をねじ込むと勝手に is-empty が挿入されるので削除する
-        document.querySelectorAll('yt-formatted-string[id="channel-id"]').forEach((elm) => {
-            elm.innerHTML = cid;
-            elm.removeAttribute('is-empty');
-        });
+        document.querySelectorAll('yt-formatted-string#channel-id.yt_og_channel_id').forEach((elm) => {
+            elm.innerHTML = cid
+            elm.removeAttribute('is-empty')
+        })
 
         // ハンドル・チャンネルIDをクリックしたらコピーする
         document.querySelectorAll('yt-formatted-string#channel-handle').forEach(elm => {
             elm.addEventListener('click', function (e) {
-                copyToClipboard(e.target.innerHTML)
-            });
-        });
+                copyToClipboard(e.target.innerHTML.trim())
+            })
+        })
+
         document.querySelectorAll('yt-formatted-string#channel-id').forEach(elm => {
             elm.addEventListener('click', function (e) {
-                copyToClipboard(e.target.innerHTML)
-            });
-        });
+                copyToClipboard(e.target.innerHTML.trim())
+            })
+        })
     }
 
 
-    function yt_open_member_list() {
+    const yt_open_member_list = () => {
         const member_ship_list = 'https://www.youtube.com/playlist?list=UUMO' + document.querySelector('yt-formatted-string#channel-id').innerText.slice(2)
         window.open(member_ship_list, '_blank')
     }
 
-
-    async function get_og_cid() {
+    const original_channel_id = async () => {
         try {
-            const response = await fetch(location.href);
-            // console.log(response.url);
-            const html = await response.text();
+            const response = await fetch(location.href)
+            const html = await response.text()
 
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(html, 'text/html')
 
-            let og_url = doc.querySelector('meta[property="og:url"]').getAttribute('content');
-            const cid = og_url.split('/')[4];
+            let og_url = doc.querySelector('meta[property="og:url"]').getAttribute('content')
+            const cid = og_url.split('/')[4]
             if (!cid) {
-                throw new Error('URLの解析に失敗しました。');
+                logger.error('URLの解析に失敗しました')
+                throw new Error('URLの解析に失敗しました')
             }
-            return cid;
+            return cid
         } catch (error) {
-            console.error('データの取得に失敗しました:', error);
-            return null;
+            logger.error('データの取得に失敗しました ' + error.message)
+            return null
         }
     }
 
-})();
+
+    /* TOOLS*/
+
+    const copyToClipboard = text => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                logger.info(`テキストがクリップボードにコピーされました: ${text}`)
+            })
+            .catch(err => {
+                logger.error('クリップボードへのアクセスに失敗しました: ' + err.message)
+                alert("クリップボードへのアクセスに失敗しました\n" + err)
+            })
+    }
 
 
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text)
-        .then(() => {
-            console.log("テキストがクリップボードにコピーされました: ", text);
-        })
-        .catch(err => {
-            console.error("クリップボードへのアクセスに失敗しました:", err);
-            alert("クリップボードへのアクセスに失敗しました\n" + err)
-        });
-}
+    const logger = new class {
+        head = '[YT-Original-channel-ID]'
+        info(msg) {
+            console.info(this.head + ' ' + msg)
+        }
+        log(msg) {
+            console.log(this.head + ' ' + msg)
+        }
+        warn(msg) {
+            console.warn(this.head + ' ' + msg)
+        }
+        error(msg) {
+            console.error(this.head + ' ' + msg)
+        }
+    }
+
+})()
 
 
