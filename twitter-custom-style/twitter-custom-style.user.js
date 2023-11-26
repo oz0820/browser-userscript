c9147ac1b6a6659e3ebbd8782295e9b8c0d59798
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-11-26 12:31:58 +0900

    画像を拡大表示したときに，画像の外側を白っぽくして境界線を見やすくする機能を追加

--- twitter-custom-style/twitter-custom-style.user.js
+++ twitter-custom-style/twitter-custom-style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Twitter custom style
 // @namespace       https://twitter.com/oz0820
-// @version         2023.11.25.0
+// @version         2023.11.26.0
 // @description     Twitterにオレオレスタイルを適用します．
 // @author          oz0820
 // @match           https://twitter.com/*
@@ -26,4 +26,48 @@ div.css-18t94o4.css-1dbjc4n.r-1777fci.r-1pl7oy7.r-1ny4l3l.r-o7ynqc.r-6416eg.r-13
 </style>`;
 
     document.head.insertAdjacentHTML('beforeend', add_style);
+
+
+
+    /* 画像を拡大表示したときに，画像の外側を白っぽくして境界線を見やすくする */
+
+    const selector = {
+        'tweetdeck.twitter.com': 'div[class="css-1dbjc4n r-1pi2tsx r-11yh6sk r-buy8e9 r-bnwqim r-13qz1uu"]',
+        'twitter.com': 'div[class="css-175oi2r r-1pi2tsx r-11yh6sk r-buy8e9 r-13qz1uu"]'
+    }
+    let path = location.pathname
+    const observer = new MutationObserver(async function (e) {
+        if (path !== location.pathname) {
+            path = location.pathname
+
+            // 画像を拡大表示しているときだけ実行
+            if (location.pathname.match(/\/\w+\/status\/\d+\/photo\/\d/)) {
+                e.forEach(elm => {
+                    const target_elm = elm.target.querySelector(selector[location.hostname])
+                    if (!!target_elm) {
+                        target_elm.style.backgroundColor = 'rgba(80, 80, 80, 0.9)'
+
+                    }
+                })
+            }
+        }
+    })
+
+    /*　
+    新しいタブを裏側で開いたとき，onloadが発火しない問題あり．要対応
+    */
+    window.onload = async () => {
+        while (true) {
+            try {
+                observer.observe(document.querySelector("#layers"), {
+                    childList: true,
+                    subtree: true,
+                })
+                break
+            } catch (e) {
+                await new Promise(resolve => setTimeout(resolve, 200))
+            }
+        }
+    }
+
 })();
