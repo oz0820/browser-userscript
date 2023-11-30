0d3598d1650351fc7211e4d776bfefe62664e92b
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-11-30 21:40:31 +0900

    DOJINDBに飛ぶボタンを追加

--- dlsite-title-copy-button/dlsite-title-copy-button.user.js
+++ dlsite-title-copy-button/dlsite-title-copy-button.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         DLSite title copy button
 // @namespace    https://twitter.com/oz0820
-// @version      2023.05.26.1
-// @description  DLSiteの作品ページで、タイトルをコピーするボタンを追加する
+// @version      2023.11.30.0
+// @description  DLSiteの作品ページで，DOJINDBに飛ぶボタンと，タイトル(作品名)をコピーするボタンを追加する
 // @author       oz0820
 // @match        https://www.dlsite.com/*/work/=/product_id/*
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/dlsite-title-copy-button/dlsite-title-copy-button.user.js
@@ -10,41 +10,74 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    window.onload = () => {
+        let h1_tag = document.querySelector('h1')
 
-    // H1タグの取得
-    let h1Tag = document.querySelector('h1');
+        /*
+        DOJINDBに飛ぶボタンを追加する
+         */
 
-    if (h1Tag) {
-        // ボタンの作成
+        // タイトルと同じ行の右側の要素のうち，一番左側の要素
+        const h1_right_elm = document.querySelector('h1').nextElementSibling
+
+        const computed_style = window.getComputedStyle(h1_right_elm)
+        const h1_right_right = Number(computed_style.getPropertyValue('right').replace(/[^0-9]/g, '')) // 要素の右からのオフセット
+        const h1_right_width = h1_right_elm.clientWidth // 横幅
+        const my_div_offset = h1_right_right + h1_right_width + 10
+
+        const dojindb_img = document.createElement('img')
+        dojindb_img.src = 'https://www.google.com/s2/favicons?sz=64&domain=dojindb.net'
+        dojindb_img.style.marginLeft = '6px'
+        dojindb_img.style.height = '26px'
+
+        const dojindb_elm = document.createElement('a')
+        dojindb_elm.href = `https://dojindb.net/rd?u=${window.location.href}`
+        dojindb_elm.target = '_blank'
+        dojindb_elm.title = 'DOJINDBに飛びます'
+        dojindb_elm.appendChild(dojindb_img)
+
+        const my_div = document.createElement('div')
+        my_div.style.position = 'absolute'
+        my_div.style.right = `${my_div_offset}px`
+        my_div.style.bottom = '10px'
+        my_div.style.marginRight = '0'
+        my_div.style.textAlign = 'right'
+        my_div.appendChild(dojindb_elm)
+
+        h1_tag.insertAdjacentElement('afterend', my_div)
+
+
+        /*
+        タイトルをコピーするボタンを追加
+         */
+
         let copyButton = document.createElement('button');
         copyButton.textContent = 'コピー';
+        copyButton.style.marginLeft = '10px'
+        copyButton.style.verticalAlign = 'top'
 
         // ボタンがクリックされたときのイベントリスナーの追加
         copyButton.addEventListener('click', function() {
             // テキストをクリップボードにコピーする
-            let textToCopy = h1Tag.textContent.slice(0, -3);
-            copyToClipboard(textToCopy);
+            let textToCopy = h1_tag.textContent.slice(0, -3);
+            _copy_to_clipboard(textToCopy);
 
             // コピーが成功した場合、メッセージを出力する
             console.log('テキストがクリップボードにコピーされました。');
         });
 
         // H1タグの後ろにボタンを挿入する
-        h1Tag.appendChild(copyButton);
+        h1_tag.appendChild(copyButton);
     }
 
-    // テキストをクリップボードにコピーする関数
-    function copyToClipboard(text) {
-        let textarea = document.createElement('textarea');
+
+    function _copy_to_clipboard(text) {
+        const textarea = document.createElement('textarea');
         textarea.value = text;
-        textarea.setAttribute('readonly', '');  // 読み取り専用に設定
-        textarea.style.position = 'fixed';  // 絶対位置に配置
-        textarea.style.opacity = 0;  // 見えないようにする
         document.body.appendChild(textarea);
         textarea.select();
         document.execCommand('copy');
         document.body.removeChild(textarea);
     }
-
-})();
+})
+();
