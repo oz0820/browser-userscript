480fcd6cfa773abf8cd054bcc937c30c6f601507
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-12-13 06:44:37 +0900

    デイリーの補助を追加

--- hoyolab-map-assistant/hoyolab-map-assistant.user.js
+++ hoyolab-map-assistant/hoyolab-map-assistant.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         テイワットマップ補助ツール
-// @version      2023.12.11.0
-// @description  テイワットマップにいくつか手を加えます
+// @name         ホヨバ補助ツール
+// @version      2023.12.13.0
+// @description  テイワットマップのピン画像を新しいタブで開く機能と，『Genshin Impact』『Honkai: Star Rail』のデイリーボタンを押す機能を追加します
 // @namespace    https://twitter.com/oz0820
 // @author       oz0820
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/hoyolab-map-assistant/hoyolab-map-assistant.user.js
 // @match        https://act.hoyolab.com/ys/app/interactive-map/index.html*
+// @match        https://act.hoyolab.com/ys/event/signin-sea-v3/index.html*
+// @match        https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hoyolab.com
 // ==/UserScript==
 
@@ -35,7 +37,7 @@
     }
 
     const GenshinImpact_daily = async () => {
-        const MAX_RETRY_TIME = 3000
+        const MAX_RETRY_TIME = 5000
         const RETRY_DELAY = 200
         const count = 0
 
@@ -44,16 +46,50 @@
             if (!sign_list) {
                 await new Promise(resolve => setTimeout(resolve, RETRY_DELAY))
             } else {
+                await new Promise(resolve => setTimeout(resolve, 200))  // ちょいと待つ
                 for (let elm of sign_list.children) {
                     elm.classList.forEach(class_name => {
                         if (class_name.startsWith('components-home-assets-__sign-content-test_---sign-wrapper')) {
                             elm.click()
+                            console.log('Genshin button find')
+                            return true
                         }
                     })
                 }
-                break
+                console.warn('Genshin button not find')
             }
         }
+        return false
+    }
+
+    const StarRail_daily = async () => {
+        const MAX_RETRY_TIME = 5000
+        const RETRY_DELAY = 200
+        const count = 0
+
+        while ((count * RETRY_DELAY) < MAX_RETRY_TIME) {
+            const sign_list = document.querySelector('div[class^="components-pc-assets-__prize-list_---list"]')
+            if (!sign_list) {
+                await new Promise(resolve => setTimeout(resolve, RETRY_DELAY))
+            } else {
+                await new Promise(resolve => setTimeout(resolve, 200))  // ちょいと待つ
+                if (!sign_list.children[0].querySelector('img[class^="components-pc-assets-__prize-list_---received"]')) {
+                    console.log('Not initialized yet')
+                    continue
+                }
+                for (let elm of sign_list.children) {
+                    if (elm.childElementCount === 3) {
+                        elm.click()
+                        console.log('StarRail button find')
+                        return true
+                    }
+                }
+                console.warn('StarRail button not find')
+                return false
+            }
+        }
+        console.warn('StarRail button not find')
+        return false
     }
 
 
@@ -62,6 +98,9 @@
     }
     if (location.href.startsWith('https://act.hoyolab.com/ys/event/signin-sea-v3/index.html')) {
         await GenshinImpact_daily()
+    }
+    if (location.href.startsWith('https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html')) {
+        await StarRail_daily()
     }
 
 })();
