660ca3e1b414f352e80f9249e10488bdbf9c3a93
oz0820 <85290191+oz0820@users.noreply.github.com>
2024-06-22 22:06:12 +0900

    不要なコードを削除

--- youtube-custom-style/youtube-custom-style.user.js
+++ youtube-custom-style/youtube-custom-style.user.js
@@ -2,7 +2,7 @@
 // @name         YouTube custom style
 // @namespace    https://twitter.com/oz0820
 // @author       oz0820
-// @version      2024.06.17.0
+// @version      2024.06.22.0
 // @description  Youtubeのスタイルを良い感じに書き換えます。
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/youtube-custom-style/youtube-custom-style.user.js
 // @match        https://www.youtube.com/*
@@ -12,57 +12,34 @@
 (async function () {
 
     // ページ移動を検出します
-    let href = window.location.href;
-    const observer = new MutationObserver(async function () {
-        if (href !== window.location.href) {
-            href = window.location.href;
+    // let href = window.location.href;
+    // const observer = new MutationObserver(async function () {
+    //     if (href !== window.location.href) {
+    //         href = window.location.href;
 
-            if (location.href.startsWith('https://www.youtube.com/watch') ||
-                location.href.startsWith('https://www.youtube.com/live')) {
-                title_font_replace();
-            }
-        }
-    })
-    observer.observe(document, {childList: true, subtree: true});
+    //         if (location.href.startsWith('https://www.youtube.com/watch') ||
+    //             location.href.startsWith('https://www.youtube.com/live')) {
+    //             title_font_replace();
+    //         }
+    //     }
+    // })
+    // observer.observe(document, {childList: true, subtree: true});
 
-    // 初回実行
-    background_color_changer();
 
-    // ライブチャットのハートマークが邪魔なので
-    const style_elm =
-        `<style>
-            yt-reaction-control-panel-overlay-view-model div#reaction-control-panel { margin-bottom: 50px; }
-            yt-reaction-control-panel-overlay-view-model yt-reaction-control-panel-view-model { opacity: 0.4; }
-            yt-reaction-control-panel-overlay-view-model yt-reaction-control-panel-view-model:hover { opacity: 1.0; }
-        </style>`;
-    if (new URL(location.href).pathname.startsWith('/live_chat')) {
+    const func_live_chat = () => {
+        // ライブチャットのハートマークが邪魔なので
+        const style_elm =
+            `<style>
+                yt-reaction-control-panel-overlay-view-model div#reaction-control-panel { margin-bottom: 50px; }
+                yt-reaction-control-panel-overlay-view-model yt-reaction-control-panel-view-model { opacity: 0.4; }
+                yt-reaction-control-panel-overlay-view-model yt-reaction-control-panel-view-model:hover { opacity: 1.0; }
+            </style>`;
         document.head.insertAdjacentHTML('beforeend', style_elm);
     }
 
-    if (location.href.startsWith('https://www.youtube.com/watch')) {
-        title_font_replace();
-    }
 
-    // 動画タイトルのフォントが重すぎてイヤなので、過去のスタイルに戻す
-
-    // ytd-watch-metadata[title-headline-xs] h1.ytd-watch-metadata {
-    //     word-break: break-word;
-    //     font-family: "YouTube Sans","Roboto",sans-serif;
-    //     font-size: 18px;
-    //     line-height: 2.8rem;
-    //     font-weight: 400;
-    //     overflow: hidden;
-    //     display: block;
-    //     max-height: 5.6rem;
-    //     -webkit-line-clamp: 2;
-    //     display: box;
-    //     display: -webkit-box;
-    //     -webkit-box-orient: vertical;
-    //     text-overflow: ellipsis;
-    //     white-space: normal;
-    // }
-
-    function title_font_replace() {
+    const func_top = () => {
+        // 動画タイトルのフォントを軽くする
         let target_elm = document.querySelector('h1.style-scope.ytd-watch-metadata');
         const css = `
         <style>
@@ -70,14 +47,16 @@
                 font-weight: 400;
             }
         </style>`
-        document.head.insertAdjacentHTML('beforeend', css)
+        document.head.insertAdjacentHTML('beforeend', css);
     }
 
-    // ダークテーマの背景を真っ黒にする
-    function background_color_changer() {
-        const before_style = document.querySelector('style[css-build-single]').textContent;
-        const after_style = before_style.replaceAll('--yt-spec-base-background: #0f0f0f', '--yt-spec-base-background: #000000');
-        document.querySelector('style[css-build-single]').textContent = after_style;
+    if (new URL(location.href).pathname.startsWith('/live_chat')) {
+        func_live_chat();
+    }
+
+    if (location.href.startsWith('https://www.youtube.com/watch') || 
+        location.href.startsWith('https://www.youtube.com/live')) {
+        func_top();
     }
 
 
