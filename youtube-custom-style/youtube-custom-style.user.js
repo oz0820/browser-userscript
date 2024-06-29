1c2d91a1180fa656eb29297e4519f916e079708e
oz0820 <85290191+oz0820@users.noreply.github.com>
2024-06-29 10:04:48 +0900

    動作の安定性を向上（したつもり

--- youtube-custom-style/youtube-custom-style.user.js
+++ youtube-custom-style/youtube-custom-style.user.js
@@ -2,7 +2,7 @@
 // @name         YouTube custom style
 // @namespace    https://twitter.com/oz0820
 // @author       oz0820
-// @version      2024.06.22.4
+// @version      2024.06.29.0
 // @description  Youtubeのスタイルを良い感じに書き換えます。
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/youtube-custom-style/youtube-custom-style.user.js
 // @match        https://www.youtube.com/*
@@ -12,18 +12,18 @@
 (async function () {
 
     // ページ移動を検出します
-    // let href = window.location.href;
-    // const observer = new MutationObserver(async function () {
-    //     if (href !== window.location.href) {
-    //         href = window.location.href;
+    let href = window.location.href;
+    const observer = new MutationObserver(async function () {
+        if (href !== window.location.href) {
+            href = window.location.href;
 
-    //         if (location.href.startsWith('https://www.youtube.com/watch') ||
-    //             location.href.startsWith('https://www.youtube.com/live')) {
-    //             title_font_replace();
-    //         }
-    //     }
-    // })
-    // observer.observe(document, {childList: true, subtree: true});
+            if (location.href.startsWith('https://www.youtube.com/watch') ||
+                location.href.startsWith('https://www.youtube.com/live')) {
+                    func_top();
+            }
+        }
+    })
+    observer.observe(document, {childList: true, subtree: true});
 
 
     const func_live_chat = () => {
@@ -64,6 +64,7 @@
         document.head.insertAdjacentHTML('beforeend', css);
     }
 
+    // 初回実行
     if (new URL(location.href).pathname.startsWith('/live_chat')) {
         func_live_chat();
     }
