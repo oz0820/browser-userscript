352df50bb4c31040634a9ee6ec89a34cb13a05bc
oz0820 <85290191+oz0820@users.noreply.github.com>
2024-07-16 03:20:40 +0900

    チャット常時表示機能のOnOffを指定する機能を追加

--- youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
+++ youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
@@ -1,15 +1,25 @@
 // ==UserScript==
 // @name         Youtube chat mode changer
 // @namespace    https://twitter.com/oz0820
-// @version      2024.06.02.0
+// @version      2024.07.16.0
 // @description  Youtubeのチャットのリプレイを常に全て表示します．
 // @author       oz0820
+// @grant        GM.setValue
+// @grant        GM.getValue
 // @match        https://www.youtube.com/*
 // @updateURL    https://github.com/oz0820/browser-userscript/raw/main/youtube-chat-mode-changer/youtube-chat-mode-changer.user.js
 // @icon         data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTU5IiBoZWlnaHQ9IjExMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCiAgPHBhdGggZD0ibTE1NCAxNy41Yy0xLjgyLTYuNzMtNy4wNy0xMi0xMy44LTEzLjgtOS4wNC0zLjQ5LTk2LjYtNS4yLTEyMiAwLjEtNi43MyAxLjgyLTEyIDcuMDctMTMuOCAxMy44LTQuMDggMTcuOS00LjM5IDU2LjYgMC4xIDc0LjkgMS44MiA2LjczIDcuMDcgMTIgMTMuOCAxMy44IDE3LjkgNC4xMiAxMDMgNC43IDEyMiAwIDYuNzMtMS44MiAxMi03LjA3IDEzLjgtMTMuOCA0LjM1LTE5LjUgNC42Ni01NS44LTAuMS03NXoiIGZpbGw9IiNmMDAiIC8+DQogIDxwYXRoIGQ9Im0xMDUgNTUtNDAuOC0yMy40djQ2Ljh6IiBmaWxsPSIjZmZmIiAvPg0KPC9zdmc+
 // ==/UserScript==
 
 (async function () {
+    // チャット常時表示機能のON・OFF
+    if (await GM.getValue('chat_replay_auto_open') === undefined) {
+        await GM.setValue('chat_replay_auto_open', true)
+        console.log('set', 'chat_replay_auto_open')
+    }
+
+    const chat_replay_auto_open = await GM.getValue('chat_replay_auto_open', true)
+
     const logger = function (message) {
         console.log('【YT-CmC】 ' + message);
     }
@@ -50,6 +60,9 @@
 
     // チャットを自動で表示する
     async function chat() {
+        if (!chat_replay_auto_open) {
+            return
+        }
         for (let i = 0; i < 20; i++) {
             document.querySelector('div#chat-container yt-button-shape > button')?.click()
             await sleep(100)
